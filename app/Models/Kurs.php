a71c4600faf351db1ecda56243f26e2602665a41
Maja Saponjic <ms20210338@student.fon.bg.ac.rs>
2024-12-20 15:46:46 +0100

    Dodati su svi modeli

---
+++ app/Models/Kurs.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kurs extends Model
+{
+    /** @use HasFactory<\Database\Factories\KursFactory> */
+    use HasFactory;
+}
