e25c73d1dfe843faca3187ae6b5fc4fa0351e08d
Dunja Stojanovic <ds20210144@student.fon.bg.ac.rs>
2024-12-26 17:46:45 +0100

    Dodate su rute za kurs

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('kursevi', [KursController::class, 'index'])->name('kursevi.index');
+    Route::get('kursevi/{id}', [KursController::class, 'show'])->name('kursevi.show');
+    Route::post('kursevi', [KursController::class, 'store'])->name('kursevi.store');
+    Route::put('kursevi/{id}', [KursController::class, 'update'])->name('kursevi.update');
+    Route::delete('kursevi/{id}', [KursController::class, 'destroy'])->name('kursevi.destroy');
+});
