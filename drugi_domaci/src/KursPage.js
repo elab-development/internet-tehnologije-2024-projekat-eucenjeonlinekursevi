eb755c112738a27e58a30107058a59b0e0f625ac
Dunja Stojanovic <ds20210144@student.fon.bg.ac.rs>
2025-02-13 12:22:28 +0100

    Kreiran je KursPage.js

---
+++ drugi_domaci/src/KursPage.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchKurs } from '../services/api';
+
+const KursPage = () => {
+  const { id } = useParams();
+  const [kurs, setKurs] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchKurs(id);
+      setKurs(data);
+    };
+
+    fetchData();
+  }, [id]);
+
+  if (!kurs) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{kurs.naziv}</h1>
+      <p>{kurs.opis}</p>
+    </div>
+  );
+};
+
+export default KursPage;
